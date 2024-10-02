5bb2c002d3a6c809d5b1e973e2ec3d0698ba72f4
dennisgarro <sistemasitm6099@gmail.com>
2024-10-02 17:01:55 -0500

    crear metodo llenar lista

---
+++ ObjProductos.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+
+public class ObjProductos {
+    String nombre = "";
+    Double precio = 0.0;
+    int cantidad = 0;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+   
+}
